77e2bbd1c680197cc6335d853d0d2be6009cde88
Sergey Stupin <stupin.sergey@gmail.com>
2025-12-13 11:28:29 +0100

    reformat

--- src/bike_geometry_comparator/sql.py
+++ src/bike_geometry_comparator/sql.py
@@ -5,9 +5,9 @@ transposed AS (PIVOT unpivoted ON size USING sum(value) GROUP BY name)
 SELECT name as param, * EXCLUDE (name) FROM transposed"""
 
 
-set_preserve_insertion_order_sql="SET preserve_insertion_order = true;"
+set_preserve_insertion_order_sql = "SET preserve_insertion_order = true;"
 
-query_sql="""
+query_sql = """
 WITH cte AS (SELECT * from 'data/canyon/endurace/2025/geometry.csv'),
 unpivoted AS (UNPIVOT cte ON COLUMNS (* EXCLUDE ('size'))),
 transposed AS (
