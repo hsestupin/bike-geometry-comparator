aa9c37f75af72743174db72ac698f74b20e378d2
Sergey Stupin <stupin.sergey@gmail.com>
2026-02-21 11:32:01 +0100

    fix 404 for database.csv

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,6 +1,6 @@
 import MainPage from './MainPage';
-import { createDataService, DataService } from "@/data/dataService";
-import { useEffect, useState } from "react";
+import {createDataService, DataService} from "@/data/dataService";
+import {useEffect, useState} from "react";
 
 const FILE_PATH = "/database.csv"
 
@@ -8,7 +8,9 @@ export default function App() {
   const [dataService, setDataService] = useState<DataService | null>(null);
 
   useEffect(() => {
-    createDataService(`${location.origin}${FILE_PATH}`).then(setDataService);
+    const baseUrl = import.meta.env.BASE_URL.replace(/\/$/, '');
+    const url = `${location.origin}${baseUrl}${FILE_PATH}`;
+    createDataService(url).then(setDataService);
   }, [])
 
   return (
