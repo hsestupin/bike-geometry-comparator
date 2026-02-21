bc650ece2ee285a5fcecce95a47b82b74852cc30
Sergey Stupin <stupin.sergey@gmail.com>
2026-02-21 11:28:19 +0100

    fix gh pages static url

--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
-import { fileURLToPath } from 'node:url';
+import {fileURLToPath} from 'node:url';
 
 const src = fileURLToPath(new URL('./src', import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/bike-geometry-comparator/',
   plugins: [react()],
   resolve: {
     alias: {
