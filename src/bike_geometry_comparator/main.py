f9af66ef55f9602c4e3c898d71346fc913ae7017
Sergey Stupin <stupin.sergey@gmail.com>
2026-02-21 11:20:14 +0100

    render table with rich instead of calling duckdb cmd

--- src/bike_geometry_comparator/main.py
+++ src/bike_geometry_comparator/main.py
@@ -1,7 +1,10 @@
 import logging
 import os
-import subprocess
 from pathlib import Path
+
+import duckdb
+from rich.console import Console
+from rich.table import Table
 
 from bike_geometry_comparator.assembly import assemble_geometry_database
 from bike_geometry_comparator.logging.colors import ColorCodes
@@ -21,7 +24,21 @@ def main() -> None:
     data_dir = Path("data")
     assemble_geometry_database(data_dir, database_file)
     logger.info(f"{ColorCodes.OKGREEN}Build succesfully finished{ColorCodes.ENDC}. Top 100 rows:")
-    subprocess.run(["duckdb", "-c", f"SELECT * FROM '{database_file}' LIMIT 100"])
+
+    con = duckdb.connect()
+    res = con.query(f"SELECT * FROM '{database_file}' LIMIT 25")
+    rows = res.fetchall()
+    columns = res.columns
+
+    table = Table(show_header=True)
+    for col in columns:
+        table.add_column(col)
+
+    for row in rows:
+        table.add_row(*[str(item) if item is not None else "" for item in row])
+
+    console = Console()
+    console.print(table)
 
 
 if __name__ == "__main__":
